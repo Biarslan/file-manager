ff81f315928662c16cb2735bbd2c4c3bfa5d7f6a
Biarslan <thebiarslan@gmail.com>
2024-02-06 01:32:36 +0300

    feat: add fs operations

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,25 +1,37 @@
 import { logCurrentDir } from '../utils/logCurrentDir.js';
 import { up, cd, ls } from './navigate.js';
-import { cat } from './operations.js';
+import { add, cat, cp, remove, rn } from './operations.js';
 
 const handleInput = async (command) => {
     console.log({ command });
     const commandArray = command.split(' ');
     const operation = commandArray[0];
-    const args = commandArray.slice(1);
+    const args = commandArray.slice(1).join(' ');
     try {
         switch (operation) {
             case 'up':
                 up();
                 break;
             case 'cd':
-                cd(args.join(' '));
+                cd(args);
                 break;
             case 'ls':
                 await ls();
                 break;
             case 'cat':
-                await cat(args.join(' '));
+                await cat(args);
+                break;
+            case 'add':
+                await add(args);
+                break;
+            case 'rn':
+                await rn(args);
+                break;
+            case 'cp':
+                await cp(args);
+                break;
+            case 'rm':
+                await remove(args);
                 break;
 
             default:
